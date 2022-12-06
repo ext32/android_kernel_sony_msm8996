7f3f28f43d0959aee595522d9f38c423e929a687
Ext3 <ext3@testbed>
2022-12-06 12:13:42 +0200

    selinux: fix double free in selinux_parse_opts_str()

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1089,8 +1089,7 @@ static int selinux_parse_opts_str(char *options,
 		goto out_err;
 
 	opts->mnt_opts_flags = kcalloc(NUM_SEL_MNT_OPTS, sizeof(int), GFP_ATOMIC);
-	if (!opts->mnt_opts_flags) {
-		kfree(opts->mnt_opts);
+	if (!opts->mnt_opts_flags)
 		goto out_err;
 	}
 
@@ -1115,6 +1114,7 @@ static int selinux_parse_opts_str(char *options,
 	return 0;
 
 out_err:
+	security_free_mnt_opts(opts);
 	kfree(context);
 	kfree(defcontext);
 	kfree(fscontext);
