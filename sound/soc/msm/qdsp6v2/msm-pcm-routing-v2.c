aacdec6e0a0db320eee63770c031b9d9763a1ee5
Ext3 <ext3@testbed>
2023-03-26 10:05:39 +0300

    Revert " qdsp6v2: msm-pcm-routing-v2: Disable custom PCM topologies"
    This reverts commit 7f7aa4ac1694df435d96fe40bc1d738828696799.

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -201,7 +201,6 @@ static bool msm_is_resample_needed(int input_sr, int output_sr)
 static void msm_pcm_routing_cfg_pp(int port_id, int copp_idx, int topology,
 				   int channels)
 {
-#if 0 /* Disable custom PCM topologies */
 	int rc = 0;
 	switch (topology) {
 	case SRS_TRUMEDIA_TOPOLOGY_ID:
@@ -253,12 +252,10 @@ static void msm_pcm_routing_cfg_pp(int port_id, int copp_idx, int topology,
 		/* custom topology specific feature param handlers */
 		break;
 	}
-#endif
 }
 
 static void msm_pcm_routing_deinit_pp(int port_id, int topology)
 {
-#if 0 /* Disable custom PCM topologies */
 	switch (topology) {
 	case SRS_TRUMEDIA_TOPOLOGY_ID:
 		pr_debug("%s: SRS_TRUMEDIA_TOPOLOGY_ID\n", __func__);
@@ -293,7 +290,6 @@ static void msm_pcm_routing_deinit_pp(int port_id, int topology)
 		/* custom topology specific feature deinit handlers */
 		break;
 	}
-#endif
 }
 
 static void msm_pcm_routng_cfg_matrix_map_pp(struct route_payload payload,
