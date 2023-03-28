1d4d7576c982b20de3d7897ed68c75ef8f613c9c
Ext3 <ext3@testbed>
2023-03-28 20:55:11 +0300

    sched/fair: Update rq clock before changing a task's CPU affinity + small missing fix from previous commits

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1347,6 +1347,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	cpumask_t allowed_mask;
 
 	rq = task_rq_lock(p, &rf);
+        update_rq_clock(rq);
 
 	/*
 	 * Must re-check here, to close a race against __kthread_bind(),
