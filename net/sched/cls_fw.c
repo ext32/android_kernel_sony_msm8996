2890e5f49c5b7cee1c97554e1f10bdbbd60c7240
Ext3 <ext3@testbed>
2023-09-12 21:41:53 +0300

    net/sched: cls_fw: Fix improper refcount update leads to use-after-free

--- net/sched/cls_fw.c
+++ net/sched/cls_fw.c
@@ -200,10 +200,6 @@ fw_change_attrs(struct net *net, struct tcf_proto *tp, struct fw_filter *f,
 	if (err < 0)
 		return err;
 
-	if (tb[TCA_FW_CLASSID]) {
-		f->res.classid = nla_get_u32(tb[TCA_FW_CLASSID]);
-		tcf_bind_filter(tp, &f->res, base);
-	}
 
 #ifdef CONFIG_NET_CLS_IND
 	if (tb[TCA_FW_INDEV]) {
@@ -226,6 +222,11 @@ fw_change_attrs(struct net *net, struct tcf_proto *tp, struct fw_filter *f,
 		goto errout;
 
 	tcf_exts_change(tp, &f->exts, &e);
+
+	if (tb[TCA_FW_CLASSID]) {
+		f->res.classid = nla_get_u32(tb[TCA_FW_CLASSID]);
+		tcf_bind_filter(tp, &f->res, base);
+	}
 
 	return 0;
 errout:
